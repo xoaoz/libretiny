5cdd6d71a1120609b26be0c724755f47e11c1236
xoaoz <xoaoz@users.noreply.github.com>
2026-08-19 11:11:56 +0800

    fix(amb1): adapt CPU information to Cortex-M3 RTL8710AF

--- cores/realtek-amb1/base/api/lt_cpu.c
+++ cores/realtek-amb1/base/api/lt_cpu.c
@@ -1,32 +1,22 @@
-/* Copyright (c) Kuba Szczodrzyński 2023-02-27. */
+/* Copyright (c) Kuba Szczodrzyński 2026. */
 
 #include <libretiny.h>
 #include <sdk_private.h>
 
 lt_cpu_model_t lt_cpu_get_model() {
-	uint8_t chipId;
-	EFUSE_OneByteReadROM(9902, 0xF8, &chipId, L25EOUTVOLTAGE);
-	return CPU_MODEL_ENUM(FAMILY, chipId);
+	return CPU_MODEL_ENUM(FAMILY, 0);
 }
 
 uint32_t lt_cpu_get_mac_id() {
-	uint32_t chipId = 0;
-	uint8_t *id		= (uint8_t *)&chipId;
-	// 9902 was extracted from ROM disassembly, probably not needed
-	/* EFUSE_OneByteReadROM(9902, 0x3B, id + 0, L25EOUTVOLTAGE);
-	EFUSE_OneByteReadROM(9902, 0x3C, id + 1, L25EOUTVOLTAGE);
-	EFUSE_OneByteReadROM(9902, 0x3D, id + 2, L25EOUTVOLTAGE); */
-	// new method, based on EFUSE logical map
-	uint8_t *efuse = (uint8_t *)malloc(512);
-	// TODO do what EFUSE_LogicalMapRead() does, and read only the used data
+	uint8_t efuse[512] = {0};
 	EFUSE_LogicalMap_Read(efuse);
-	memcpy(id, efuse + 0x11A + 3, 3);
-	free(efuse);
-	return chipId;
+	uint32_t id = 0;
+	memcpy(&id, efuse + 0x11A + 3, 3);
+	return id;
 }
 
 const char *lt_cpu_get_core_type() {
-	return "ARM Cortex-M4F (ARMv7E-M)";
+	return "ARM Cortex-M3 (ARMv7-M)";
 }
 
 uint32_t lt_cpu_get_freq() {
