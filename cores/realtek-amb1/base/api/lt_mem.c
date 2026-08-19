8a5abd5c114db89eb53038f04abb509876385c90
xoaoz <xoaoz@users.noreply.github.com>
2026-08-19 11:12:32 +0800

    fix(amb1): report RTL8710AF available RAM

--- cores/realtek-amb1/base/api/lt_mem.c
+++ cores/realtek-amb1/base/api/lt_mem.c
@@ -1,8 +1,7 @@
-/* Copyright (c) Kuba Szczodrzyński 2023-02-27. */
+/* Copyright (c) Kuba Szczodrzyński 2026. */
 
 #include <libretiny.h>
-#include <sdk_private.h>
 
 uint32_t lt_ram_get_size() {
-	return 256 * 1024;
+	return 434176;
 }
